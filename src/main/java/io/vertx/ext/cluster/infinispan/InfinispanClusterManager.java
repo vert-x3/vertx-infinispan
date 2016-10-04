vert-x3/vertx-infinispan

commit 7faa21c990b1ab8a57a3e7225eef5899cbdd6d30

Do not stop cache manager if provided by user

src/main/java/io/vertx/ext/cluster/infinispan/InfinispanClusterManager.java
@@ -193,7 +193,9 @@ public class InfinispanClusterManager implements ClusterManager {
         return;
       }
       active = false;
-      cacheManager.stop();
+      if (configPath == null) {
+        cacheManager.stop();
+      }
       future.complete();
     }, false, resultHandler);
   }

commit 719508f295eaca23c7a8299790885384167743f6

Should stop the cache manager if it's not provided by the user
Fixes bug introduced by 7faa21c

src/main/java/io/vertx/ext/cluster/infinispan/InfinispanClusterManager.java
@@ -193,7 +193,7 @@ public class InfinispanClusterManager implements ClusterManager {
         return;
       }
       active = false;
-      if (configPath == null) {
+      if (configPath != null) {
         cacheManager.stop();
       }
       future.complete();