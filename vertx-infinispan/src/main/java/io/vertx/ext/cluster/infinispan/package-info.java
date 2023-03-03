784b7e41a8d127b6e19d5db1501cc39036402a54
Thomas Segismont <tsegismont@gmail.com>
2023-03-03 16:31:28 +0100

    Deprecate callbacks
    See vert-x3/issues#612
    
    Easy one, no callbacks in this module
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- vertx-infinispan/src/main/java/io/vertx/ext/cluster/infinispan/package-info.java
+++ vertx-infinispan/src/main/java/io/vertx/ext/cluster/infinispan/package-info.java
@@ -14,7 +14,7 @@
  * under the License.
  */
 
-@ModuleGen(name = "vertx-infinispan", groupPackage = "io.vertx")
+@ModuleGen(name = "vertx-infinispan", groupPackage = "io.vertx", checkCallbackDeprecation = true)
 package io.vertx.ext.cluster.infinispan;
 
 import io.vertx.codegen.annotations.ModuleGen;
